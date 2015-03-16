2236e595ef9fd76f547f3a10a57447c382caac49
Dominik Honnef <dominik@honnef.co>
2015-03-17 00:06:42 +0100

    move writing tag header to Encoder

--- encoder.go
+++ encoder.go
@@ -13,6 +13,12 @@ func NewEncoder(w io.Writer) *Encoder {
 	return &Encoder{w: w}
 }
 
+func (e *Encoder) WriteHeader(size int) error {
+	h := generateHeader(size)
+	_, err := e.w.Write(h)
+	return err
+}
+
 func (e *Encoder) WriteFrame(f Frame) error {
 	b := f.Header().serialize(f.size() - frameLength)
 	_, err := e.w.Write(b)
@@ -26,13 +32,11 @@ func (e *Encoder) WriteFrame(f Frame) error {
 
 func (t *Tag) Encode(w io.Writer) error {
 	t.SetTextFrameTime("TDTG", time.Now().UTC())
-	header := generateHeader(t.Frames.size() + Padding)
-	_, err := w.Write(header)
+	enc := NewEncoder(w)
+	err := enc.WriteHeader(t.Frames.size() + Padding)
 	if err != nil {
 		return err
 	}
-
-	enc := NewEncoder(w)
 
 	// TODO write important frames first
 	for _, frames := range t.Frames {
